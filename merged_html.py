71dada58867b93e209794537d090c8d09b5f6a50
Jesus Fernandez <Jesus.Fernandez@unican.es>
2024-07-09 14:46:22 +0200

    Publish CSV version of the merged data request

--- merged_html.py
+++ merged_html.py
@@ -17,13 +17,13 @@ data_request_fpsurbrcc['priority-urb'] = 'URB-' + data_request_fpsurbrcc['priori
 data_request_fpsconv.rename(columns={'priority': 'priority-conv'}, inplace=True)
 data_request_fpsconv['priority-conv'] = 'CONV-' + data_request_fpsconv['priority-conv'].astype(str)
 
+columns_all = ['units', 'long_name', 'standard_name', 'priority', 'priority-urb', 'priority-conv', 'comment', 'cell_measures', 'cell_methods', 'dimensions', 'ok_max_mean_abs', 'ok_min_mean_abs', 'positive', 'type', 'valid_max', 'valid_min']
+columns_display = ['units', 'long_name', 'priority', 'priority-urb', 'priority-conv', 'comment']
+
 merged_df = data_request.combine_first(data_request_fpsurbrcc).combine_first(data_request_fpsconv)
-(
-    merged_df
-        .loc[:,['units', 'long_name', 'priority', 'priority-urb', 'priority-conv', 'comment']]
-        .reset_index()
-        .to_csv(csvout, index=False)
-)
+merged_df.loc[:,columns_all].reset_index().to_csv(f'docs/{csvout}', index=False)
+merged_df.loc[:,columns_display].reset_index().to_csv(csvout, index=False)
+
 
 csv2datatable(
     csvout,
@@ -34,6 +34,7 @@ csv2datatable(
         from the different I4C work packages and includes also the variables requested from the
         <a href="https://github.com/impetus4change/T32-CPRCM/blob/main/data-request-fpsurbrcc.csv">CORDEX FPS-URB-RCC</a> and
         <a href="https://github.com/impetus4change/T32-CPRCM/blob/main/data-request-fpsconv.csv">FPS-CONV</a> initiatives.
+        This merged data request table is also available as a [CSV file](https://impetus4change.github.io/T32-CPRCM/data-request-i4c-merged.csv).
         Note: To avoid duplicated entries, the frequency shown for some variables in the data request from the
         FPS-CONV initiative has been adapted to match the highest frequency requested by the other two ongoing
         activities.''',
